6b7b2fc85aa4ff978fa25b8238b895ac53e37917
Javier Yuste <javierym8@gmail.com>
2019-02-16 12:41:25 +0100

    Schema drafted

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
-
+# TODO pprint is useful to print json and dictionary-based structures, but will not make its way to the final release.
+# Remove this prior to final release
+import pprint
 
 class bcolors:
     HEADER = '\033[95m'
@@ -35,23 +37,76 @@ class utilities():
         if not self.quiet:
             print(string)
 
+    # TODO pprint is useful to print json and dictionary-based structures, but will not make its way to the final release.
+    # Remove this prior to final release
     def pprint(self, lines_data, output_file):
         if not output_file:
             pp = pprint.PrettyPrinter(indent=4)
             pp.pprint(lines_data)
 
-def main(A1, A2):
+def transform_DFA(A):
+    '''
+    Checks if <A> is a DFA and transforms it into a Deterministic Finite Automata if its not
+    :param A: an automata
+    :output A': a DFA
+    '''
+    pass
 
+def complete_DFA(A):
+    '''
+    Transforms <A> into a complete DFA
+    :param A: a DFA
+    :output A': a complete DFA
+    '''
+    pass
+
+def obtain_complement(A):
+    '''
+    Obtains A's complement
+    :param A: a complete DFA
+    :output B: A's complement
+    '''
+    pass
+
+def obtain_intersection(A1, A2):
+    '''
+    Obtains the automata, named I, resulting from the intersection of A1 and A2
+    :param A1: a complete DFA
+    :param A2: a complete DFA
+    :output I: intersection of A1 and A2
+    '''
+    pass
+
+def is_empty(A):
+    '''
+    Checks if the accepted language of a given automata is empty/none
+    :param A: an automata
+    :output boolean: True if its language is empty, False otherwise
+    '''
+    pass
+
+def main(A1, A2):
+    '''
+    This method will control the execution flow of the algorithm, consecutively calling the required methods to state if A1 and A2 are equal
+    :param A1: an automata
+    :param A2: an automata
+    :output boolean: True if equal, False otherwise
+    '''
+    pass
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path-chromedriver", help="Path where chromedriver is stored", type=str,
-                        default=PATH_CHROMEDRIVER)
-    parser.add_argument("-o", "--output", help="If not specified, no output will be produced", action="store_true")
-    parser.add_argument("-nc", "--no-color", help="Do not colorize output", action="store_true")
-    parser.add_argument("-q", "--quiet", help="Quiet, no output on terminal", action="store_true")
-    parser.add_argument("-v", "--verbose",
-                        help="Be verbose. Useful when performing on slow network connections to know that it is working "
-                            "and not dead",
-                        action="store_true")
+    # TODO: make this try-except block decent
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-nc", "--no-color", help="Do not colorize output", action="store_true")
+        parser.add_argument("-q", "--quiet", help="Only True or False is printed", action="store_true")
+        parser.add_argument("-v", "--verbose",
+                            help="Print information as the algorithm progresses",
+                            action="store_true")
+        args = parser.parse_args()
+        global util
+        util = utilities(args)
+    except Exception as e:
+        util.print_color("[-] An error ocurred!", bcolors.FAIL)
+        util.print_verbose(e)
     
